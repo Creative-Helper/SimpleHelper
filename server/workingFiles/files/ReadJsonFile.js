ceb070dbbc351f71de0b62dac59d06fe294a9a08
infogon <godan2310@yahoo.es>
2021-03-21 04:16:08 +0100

    migration to yarn 2

--- server/workingFiles/files/ReadJsonFile.js
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs'
-
-const readFileJson = (path, file) => {
-  const fileJson = readFileSync(path + file)
-  return JSON.parse(fileJson)
-}
-
-export default readFileJson
